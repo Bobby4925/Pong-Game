d1f45b299f1928f1c80c845f04544a0b6b0c68ae
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-05-05 00:33:29 -0700

    Added Paddles

--- Pong/Game1.cs
+++ Pong/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Linq;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,9 +7,14 @@ namespace Pong;
 
 public class Game1 : Game
 {
-    Texture2D balltexture;
+    Texture2D paddleTexture;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Rectangle paddleRect1;
+    private Rectangle paddleRect2;
+    private int windowWidth;
+    private int windowHeight;
+    private int paddleSpeed = 5;
 
     public Game1()
     {
@@ -20,14 +26,18 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-
+        windowWidth = Window.ClientBounds.Width;
+        windowHeight = Window.ClientBounds.Height;
+        paddleRect1 = new Rectangle(10,120,30,90);
+        paddleRect2 = new Rectangle(windowWidth - 50,120,30,90);
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+        paddleTexture = new Texture2D(GraphicsDevice, 3, 3);
+        paddleTexture.SetData<Color>(Enumerable.Repeat(Color.White, 3 * 3).ToArray());
         // TODO: use this.Content to load your game content here
     }
 
@@ -37,13 +47,24 @@ public class Game1 : Game
             Exit();
 
         // TODO: Add your update logic here
-
+        KeyboardState keyboard = Keyboard.GetState();
+        if(keyboard.IsKeyDown(Keys.W)){
+            paddleRect1.Y -= paddleSpeed;
+        }
+        else if(keyboard.IsKeyDown(Keys.S)){
+            paddleRect1.Y += paddleSpeed; 
+        }
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin(); 
+        _spriteBatch.Draw(paddleTexture,paddleRect1, Color.White);
+        _spriteBatch.Draw(paddleTexture,paddleRect2, Color.White);
+        _spriteBatch.End(); 
 
         // TODO: Add your drawing code here
 
